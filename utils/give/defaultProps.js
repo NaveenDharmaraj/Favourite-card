df150d3d540e7a3be1de04022e9a94c56ca457df
Arun C P <aruncp123@gmail.com>
2019-06-27 10:41:26 +0530

    Added flow logic for give to charity

--- utils/give/defaultProps.js
+++ utils/give/defaultProps.js
@@ -31,3 +31,92 @@ export const donationDefaultProps = {
         type: 'donations',
     },
 };
+
+export const beneficiaryDefaultProps = {
+    flowObject: {
+        currency: 'USD',
+        giveData: {
+            coverFees: false,
+            coverFeesAmount: null,
+            creditCard: {
+                value: null,
+            },
+            donationAmount: '',
+            donationMatch: {
+                value: null,
+            },
+            giftType: {
+                value: 0,
+            },
+            giveAmount: '',
+            giveFrom: {
+                value: '',
+            },
+            giveTo: {
+                value: null,
+            },
+            infoToShare: {
+                value: 'anonymous',
+            },
+            newCreditCardId: null,
+            noteToCharity: '',
+            noteToSelf: '',
+            userInteracted: false,
+        },
+        groupFromUrl: false,
+        groupId: null,
+        nextSteptoProceed: {},
+        selectedTaxReceiptProfile: {},
+        slugValue: null,
+        sourceAccountHolderId: null,
+        stepsCompleted: false,
+        taxReceiptProfileAction: 'no_change',
+        type: 'give/to/charity',
+    },
+};
+
+export const groupDefaultProps = {
+    flowObject: {
+        currency: 'USD',
+        giveData: {
+            coverFees: false,
+            coverFeesAmount: null,
+            creditCard: {
+                value: null,
+            },
+            donationAmount: '',
+            donationMatch: {
+                value: null,
+            },
+            giftType: {
+                value: 0,
+            },
+            giveAmount: '',
+            giveFrom: {
+                value: '',
+            },
+            giveTo: {
+                value: null,
+            },
+            infoToShare: {
+                value: null,
+            },
+            noteToCharity: '',
+            noteToSelf: '',
+            privacyShareAddress: false,
+            privacyShareAmount: true,
+            privacyShareEmail: false,
+            privacyShareName: true,
+            userInteracted: false,
+        },
+        groupFromUrl: false,
+        groupIndex: 0,
+        nextSteptoProceed: {},
+        selectedTaxReceiptProfile: {},
+        slugValue: null,
+        sourceAccountHolderId: null,
+        stepsCompleted: false,
+        taxReceiptProfileAction: 'no_change',
+        type: 'give/to/group',
+    },
+};
