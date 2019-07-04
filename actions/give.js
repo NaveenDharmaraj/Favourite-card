45e06d2016f8b8e23b51b14ecc81497b9da1a3fa
Aravinth-0812 <aravinth.periasamy@chimpfund.com>
2019-07-04 17:08:01 +0530

    fixed lint issues

--- actions/give.js
+++ actions/give.js
@@ -1,10 +1,5 @@
 import coreApi from '../services/coreApi';
-import {
-    beneficiaryDefaultProps,
-    donationDefaultProps,
-    p2pDefaultProps,
-    groupDefaultProps,
-} from '../utils/give/defaultProps';
+
 export const actionTypes = {
     SAVE_FLOW_OBJECT: 'SAVE_FLOW_OBJECT'
 }
@@ -22,11 +17,3 @@ export const reInitNextStep = (dispatch, flowObject) => {
     flowObject.nextStep = null;
     return dispatch({type: actionTypes.SAVE_FLOW_OBJECT, payload: flowObject})
 }
-
-export const clearFlowObjects = (flowObject) => {
-    flowObject.nextStep = null;
-    const clearFlowObject = Object.assign(flowObject,beneficiaryDefaultProps,donationDefaultProps,p2pDefaultProps,groupDefaultProps);
-    console.log("clearFlowObjects",clearFlowObject);
-    proceed(clearFlowObject);
-}
-
