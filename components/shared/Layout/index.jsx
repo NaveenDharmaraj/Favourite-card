3d9280c66c9b56136c5f8ee6d5f778712b5afaaf
Venu <venu.hg@chimp.net>
2019-07-26 13:59:19 +0530

    Removed Link from Layout

--- components/shared/Layout/index.jsx
+++ components/shared/Layout/index.jsx
@@ -55,14 +55,6 @@ class Layout extends React.Component {
                     </Responsive>
                     <Responsive minWidth={Responsive.onlyTablet.minWidth}>
                         <Header isAuthenticated={isAuthenticated} />
-                        <Link route="/donations/new">
-                                   ADD
-                    </Link>
-                     <br/>
-                     <br/>
-                    <Link route="/give/to/charity/the-canadian-red-cross-society-la-societe-canadienne-de-la-croix-rouge/gift/new">
-                                   GIVE
-                    </Link>
                          <Container><div className="pageWraper">{children}</div></Container>
                     </Responsive>
                     <Footer />
