deea78c6690a2a306fd5cd65d16bb15ecbea0954
AKHIL P MURALI <itsakhilpm@gmail.com>
2019-07-26 09:56:39 +0530

    changing causes to seperate component

--- pages/users/new/CauseComponent.jsx
+++ pages/users/new/CauseComponent.jsx
@@ -8,14 +8,21 @@ import {
 } from 'semantic-ui-react';
 import _ from 'lodash';
 
-import FormValidationErrorMessage from '../../../components/shared/FormValidationErrorMessage';
-
 function CauseComponent(props) {
     let {
         cause,
         parentHandleCauses,
         userCauses,
     } = props;
+    function titleCase(str) {
+        const splitStr = str.replace(/_/g, ' ').toLowerCase().split(' ');
+        for (let i = 0; i < splitStr.length; i++) {
+            splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+        }
+        return splitStr.join(' ');
+    }
+    const capStrng = titleCase(cause);
+
     return (
         <Grid.Column mobile={16} tablet={8} computer={4}>
             <Button
@@ -23,13 +30,13 @@ function CauseComponent(props) {
                 fluid
                 className={`select-btn ${_.includes(userCauses, cause) ? 'active' : ''}`}
                 id={cause}
-                name="arts_and_culture"
+                name={cause}
                 onClick={parentHandleCauses}
             >
-                {cause}
+                {capStrng}
             </Button>
         </Grid.Column>
-    )
+    );
 }
 
 export default CauseComponent;
--- pages/users/new/CausesComponent.jsx
+++ pages/users/new/CausesComponent.jsx
@@ -33,22 +33,19 @@ function CausesComponent(props) {
         'youth_or_children',
     ];
     const renderCauses = () =>{
-        // if(!_.isEmpty(causesArray)){
-        let causesBlock = null;
-        if (true) {
+        const causesBlock = [];
+        if (!_.isEmpty(causesArray)) {
             causesArray.forEach((cause) => {
-                console.log(cause);
-                    <CauseComponent 
-                        parentHandleCauses={parentHandleCauses}
-                        userCauses={userCauses}
-                        cause={cause}
-                    />
+                causesBlock.push(<CauseComponent
+                    parentHandleCauses={parentHandleCauses}
+                    userCauses={userCauses}
+                    cause={cause}
+                />);
             });
         }
         return causesBlock;
     };
-    return (
-        // renderCauses()
+    return (        
         <Grid.Column mobile={16} tablet={14} computer={14} largeScreen={12}>
             <div className="prefered-wraper">
                 <div className="prefered-img" />
@@ -59,152 +56,7 @@ function CausesComponent(props) {
                 <p>Choose 3 or more</p>
                 <Grid className="select-btn-wraper">
                     <Grid.Row>
-                        <Grid.Column mobile={16} tablet={8} computer={4}>
-                            <Button
-                                basic
-                                fluid
-                                className={`select-btn ${_.includes(userCauses, 'arts_and_culture') ? 'active' : ''}`}
-                                id="arts_and_culture"
-                                name="arts_and_culture"
-                                onClick={parentHandleCauses}
-                            >
-                                Arts and culture
-                            </Button>
-                        </Grid.Column>
-                        <Grid.Column mobile={16} tablet={8} computer={4}>
-                            <Button
-                                basic
-                                fluid
-                                className={`select-btn ${_.includes(userCauses, 'health') ? 'active' : ''}`}
-                                id="health"
-                                name="health"
-                                onClick={parentHandleCauses}
-                            >
-                                Health
-                            </Button>
-                        </Grid.Column>
-                        <Grid.Column mobile={16} tablet={8} computer={4}>
-                            <Button
-                                basic
-                                fluid
-                                className={`select-btn ${_.includes(userCauses, 'education_and_research') ? 'active' : ''}`}
-                                id="education_and_research"
-                                name="education_and_research"
-                                onClick={parentHandleCauses}
-                            >
-                                Education and research
-                            </Button>
-                        </Grid.Column>
-                        <Grid.Column mobile={16} tablet={8} computer={4}>
-                            <Button
-                                basic
-                                fluid
-                                className={`select-btn ${_.includes(userCauses, 'religion_and_spirituality') ? 'active' : ''}`}
-                                id="religion_and_spirituality"
-                                name="religion_and_spirituality"
-                                onClick={parentHandleCauses}
-                            >
-                                Religion and spirituality
-                            </Button>
-                        </Grid.Column>
-
-                        <Grid.Column mobile={16} tablet={8} computer={4}>
-                            <Button
-                                basic
-                                fluid
-                                className={`select-btn ${_.includes(userCauses, 'human_rights') ? 'active':''}`}
-                                id="human_rights"
-                                name="human_rights"
-                                onClick={parentHandleCauses}
-                            >
-                                Human rights
-                            </Button>
-                        </Grid.Column>
-                        <Grid.Column mobile={16} tablet={8} computer={4}>
-                            <Button
-                                basic
-                                fluid
-                                className={`select-btn ${_.includes(userCauses, 'international') ? 'active' : ''}`}
-                                id="international"
-                                name="international"
-                                onClick={parentHandleCauses}
-                            >
-                                International
-                            </Button>
-                        </Grid.Column>
-                        <Grid.Column mobile={16} tablet={8} computer={4}>
-                            <Button
-                                basic
-                                fluid
-                                className={`select-btn ${_.includes(userCauses, 'environment') ? 'active' : ''}`}
-                                id="environment"
-                                name="environment"
-                                onClick={parentHandleCauses}
-                            >
-                                Environment
-                            </Button>
-                        </Grid.Column>
-                        <Grid.Column mobile={16} tablet={8} computer={4}>
-                            <Button
-                                basic
-                                fluid
-                                className={`select-btn ${_.includes(userCauses, 'sports_and_recreation') ? 'active' : ''}`}
-                                id="sports_and_recreation"
-                                name="sports_and_recreation"
-                                onClick={parentHandleCauses}
-                            >
-                                Sports and recreation
-                            </Button>
-                        </Grid.Column>
-
-                        <Grid.Column mobile={16} tablet={8} computer={4}>
-                            <Button
-                                basic
-                                fluid
-                                className={`select-btn ${_.includes(userCauses, 'outreach_and_welfare') ? 'active' : ''}`}
-                                id="outreach_and_welfare"
-                                name="outreach_and_welfare"
-                                onClick={parentHandleCauses}
-                            >
-                                Outreach and welfare
-                            </Button>
-                        </Grid.Column>
-                        <Grid.Column mobile={16} tablet={8} computer={4}>
-                            <Button
-                                basic
-                                fluid
-                                className={`select-btn ${_.includes(userCauses, 'animals') ? 'active' : ''}`}
-                                onClick={parentHandleCauses}
-                                id="animals"
-                                name="animals"
-                            >
-                                Animals
-                            </Button>
-                        </Grid.Column>
-                        <Grid.Column mobile={16} tablet={8} computer={4}>
-                            <Button
-                                basic
-                                fluid
-                                className={`select-btn ${_.includes(userCauses, 'community_development') ? 'active' : ''}`}
-                                id="community_development"
-                                name="community_development"
-                                onClick={parentHandleCauses}
-                            >
-                                Community development
-                            </Button>
-                        </Grid.Column>
-                        <Grid.Column mobile={16} tablet={8} computer={4}>
-                            <Button
-                                basic
-                                fluid
-                                className={`select-btn ${_.includes(userCauses, 'youth_or_children') ? 'active' : ''}`}
-                                id="youth_or_children"
-                                name="youth_or_children"
-                                onClick={parentHandleCauses}
-                            >
-                                Youth/children
-                            </Button>
-                        </Grid.Column>
+                        {renderCauses()}
                     </Grid.Row>
                 </Grid>
                 <FormValidationErrorMessage
