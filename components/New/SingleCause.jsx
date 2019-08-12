a6aa9e91d935c71487f1ec7e27a42338e9e14441
Aravinth <aravinth.periasamy@chimpfund.com>
2019-08-12 22:43:40 +0530

    Task/causes api integration (#122)
    * Causes_api_integration
    
    * removed from reducer

--- components/New/SingleCause.jsx
+++ components/New/SingleCause.jsx
@@ -11,29 +11,28 @@ import _ from 'lodash';
 function SingleCause(props) {
     let {
         cause,
+        index,
         parentHandleCauses,
         userCauses,
     } = props;
-    function titleCase(str) {
-        const splitStr = str.replace(/_/g, ' ').toLowerCase().split(' ');
-        for (let i = 0; i < splitStr.length; i++) {
-            splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
-        }
-        return splitStr.join(' ');
-    }
-    const causeName = titleCase(cause);
 
+    const {
+        attributes: {
+            display_name,
+            name,
+        },
+    } = cause;
     return (
         <Grid.Column mobile={16} tablet={8} computer={4}>
             <Button
                 basic
                 fluid
-                className={`select-btn ${_.includes(userCauses, cause) ? 'active' : ''}`}
-                id={cause}
-                name={cause}
+                className={`select-btn color_${index} ${_.includes(userCauses, name) ? 'active' : ''}`}
+                id={name}
+                name={name}
                 onClick={parentHandleCauses}
             >
-                {causeName}
+                {display_name}
             </Button>
         </Grid.Column>
     );
