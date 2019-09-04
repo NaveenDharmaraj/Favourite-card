af397f161e04c0383bf870d75d95b7cc1f40a1c2
AKHIL P MURALI <itsakhilpm@gmail.com>
2019-09-04 12:06:27 +0530

    bug fix for find by slug api failure

--- actions/profile.js
+++ actions/profile.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-else-return */
 import _ from 'lodash';
 
-import { Router } from '../routes';
 import coreApi from '../services/coreApi';
 import utilityApi from '../services/utilityApi';
 import graphApi from '../services/graphApi';
@@ -51,6 +50,7 @@ export const actionTypes = {
     GET_SUB_GROUPS_FOR_CAMPAIGN: 'GET_SUB_GROUPS_FOR_CAMPAIGN',
     SAVE_FOLLOW_STATUS_CAMPAIGN: 'SAVE_FOLLOW_STATUS_CAMPAIGN',
     SEE_MORE_LOADER: 'SEE_MORE_LOADER',
+    SLUG_API_ERROR_STATUS: 'SLUG_API_ERROR_STATUS',
     SUB_GROUP_LIST_LOADER: 'SUB_GROUP_LIST_LOADER',
 };
 
@@ -115,7 +115,13 @@ export const getCampaignFromSlug = async (dispatch, slug) => {
         },
     ).catch((error) => {
         console.log(error);
-        Router.pushRoute('/give/error');
+        dispatch({
+            payload: {
+                slugApiErrorStats: true,
+            },
+            type: actionTypes.SLUG_API_ERROR_STATUS,
+        });
+        // Router.pushRoute('/give/error');
     });
 };
 
