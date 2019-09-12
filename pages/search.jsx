63a362dd85e2ab724a0f0501d9278ee45e610516
Aravinth-0812 <aravinth.periasamy@chimpfund.com>
2019-09-12 15:32:19 +0530

    decodeuri

--- pages/search.jsx
+++ pages/search.jsx
@@ -69,11 +69,11 @@ class Search extends React.Component {
         } else if (_isEmpty(searchWord) && searchType === 'Group') {
             dispatch(fetchInitialGroups(currentPageClicked, isAuthenticated, id));
         } else if (!_isEmpty(searchWord) && searchType === 'All') {
-            dispatch(fetchTextSearchCharitiesGroups(searchWord, currentPageClicked));
+            dispatch(fetchTextSearchCharitiesGroups(decodeURI(searchWord), currentPageClicked));
         } else if (!_isEmpty(searchWord) && searchType === 'Beneficiary') {
-            dispatch(fetchTextSearchCharities(searchWord, currentPageClicked));
+            dispatch(fetchTextSearchCharities(decodeURI(searchWord), currentPageClicked));
         } else {
-            dispatch(fetchTextSearchGroups(searchWord, currentPageClicked));
+            dispatch(fetchTextSearchGroups(decodeURI(searchWord), currentPageClicked));
         }
     }
 
@@ -139,21 +139,21 @@ class Search extends React.Component {
                         if (_isEmpty(searchWord)) {
                             dispatch(fetchInitialCharitiesGroups(isAuthenticated, id));
                         } else {
-                            dispatch(fetchTextSearchCharitiesGroups(searchWord, currentPageClicked, filterData));
+                            dispatch(fetchTextSearchCharitiesGroups(decodeURI(searchWord), currentPageClicked, filterData));
                         }
                         break;
                     case 'Beneficiary':
                         if (_isEmpty(searchWord)) {
                             dispatch(fetchInitialCharities(currentPageClicked, isAuthenticated, id));
                         } else {
-                            dispatch(fetchTextSearchCharities(searchWord, currentPageClicked, filterData));
+                            dispatch(fetchTextSearchCharities(decodeURI(searchWord), currentPageClicked, filterData));
                         }
                         break;
                     case 'Group':
                         if (_isEmpty(searchWord)) {
                             dispatch(fetchInitialGroups(currentPageClicked, isAuthenticated, id));
                         } else {
-                            dispatch(fetchTextSearchGroups(searchWord, currentPageClicked, filterData));
+                            dispatch(fetchTextSearchGroups(decodeURI(searchWord), currentPageClicked, filterData));
                         }
                         break;
                     default: break;
