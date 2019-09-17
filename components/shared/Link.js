33284ea96155eae0f2d833a4a3d036183c79accd
Aravinth-0812 <aravinth.periasamy@chimpfund.com>
2019-09-17 16:29:07 +0530

    added Link file

---
+++ components/shared/Link.js
@@ -0,0 +1,24 @@
+import { withRouter } from 'next/router';
+import React, {
+    Children,
+    cloneElement,
+} from 'react';
+
+import { Link } from '../../routes';
+
+const ActiveLink = ({
+    router, children, ...props
+}) => {
+    const child = Children.only(children);
+
+    let className = child.props.className || '';
+    if (router.pathname === (props.href || props.route) && props.activeClassName) {
+        className = `${className} ${props.activeClassName}`.trim();
+    }
+
+    delete props.activeClassName;
+
+    return <Link {...props}>{cloneElement(child, { className })}</Link>;
+};
+
+export default withRouter(ActiveLink);
