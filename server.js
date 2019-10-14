83d886abc7d575ddbbf72d241f8c41a6686e50b2
Gijo Raj <gijo.raj@chimp.net>
2019-10-14 23:29:50 +0530

    Removing the trailing back slash from the req url (#513)

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
-const express = require('express')
-const next = require('next')
-const routes = require('./routes')
-const app = next({dev: process.env.NODE_ENV !== 'production'})
-const nextI18NextMiddleware = require('next-i18next/middleware')
-const nextI18next = require('./i18n')
-const handler = routes.getRequestHandler(app)
+const express = require('express');
+const next = require('next');
+const routes = require('./routes');
+const app = next({dev: process.env.NODE_ENV !== 'production'});
+const nextI18NextMiddleware = require('next-i18next/middleware');
+const nextI18next = require('./i18n');
+const handler = routes.getRequestHandler(app);
 // With express
 const port = (process.env.PORT) ? process.env.PORT : 3000;
 app.prepare().then(() => {
     const server = express();
-    server.get('/static/favicon.ico', (req, res) => (
-        res.status(200).sendFile('favicon.ico', {root: __dirname + '/static/'})
-    ));
- 
+    const handleTrailingSlashes = (req, res, nxt) => {
+        const test = /\?[^]*\//.test(req.url);
+        if (req.url.substr(-1) === '/' && req.url.length > 1 && !test) {
+            res.redirect(301, req.url.slice(0, -1));
+        } else {
+            nxt();
+        }
+    };
+    server.use(handleTrailingSlashes);
     server.use(nextI18NextMiddleware(nextI18next));
     server.use(handler);
     server.listen(process.env.PORT || 3000);
