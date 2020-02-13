d64ed37aec8bd635e2e54f26322733ecf0487203
Gijo Raj <gijoraj@gmail.com>
2020-02-13 11:20:35 +0530

    bugherd 274 pagination style change

--- components/shared/Pagination/index.jsx
+++ components/shared/Pagination/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Pagination } from 'semantic-ui-react';
+import { Pagination,Icon } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 const PaginationComponent = (props) => {
     const {
@@ -16,10 +16,12 @@ const PaginationComponent = (props) => {
             onPageChange={onPageChanged}
             activePage={activePage}
             totalPages={totalPages}
-            firstItem={(activePage === 1) ? null : undefined}
-            lastItem={(activePage === totalPages) ? null : undefined}
-            prevItem={(activePage === 1) ? null : undefined}
-            nextItem={(activePage === totalPages) ? null : undefined}
+            firstItem={false}
+            lastItem={false}
+            // firstItem={(activePage === 1) ? null : undefined}
+            // lastItem={(activePage === totalPages) ? null : undefined}
+            prevItem={{ disabled: (activePage === 1), content: <Icon name='angle left' />, icon: true }}
+            nextItem={{ disabled: (activePage === totalPages), content: <Icon name='angle right' />, icon: true }}
         />
     );
 };
