88bb84de7c9cea42b89387564f90bf408f4e2e98
tsfahmed <tousif.ahmed@nirman.io>
2020-02-26 13:35:19 +0530

    modifying to class component

--- components/shared/LeftImageCard/index.jsx
+++ components/shared/LeftImageCard/index.jsx
@@ -12,66 +12,80 @@ import _ from 'lodash';
 import { Link } from '../../../routes';
 import { renderTextByCharacter } from '../../../helpers/utils';
 
-const LeftImageCard = (props) => {
-    const {
-        buttonState,
-        changeButtonState,
-        entityName,
-        location,
-        placeholder,
-        typeClass,
-        type,
-        url,
-    } = props;
-    const entityShortName = renderTextByCharacter(entityName, 25);
-    return (
-        <Grid.Column>
-            <Card className="left-img-card" fluid>
-                <Card.Header>
-                    <Grid>
-                        <Grid.Row>
-                            <Grid.Column width={6}>
-                                <Image src={placeholder} />
-                            </Grid.Column>
-                            <Grid.Column width={10}>
-                                <div style={{padding:'1rem 0rem 0.2rem'}}>
-                                    <Header as="h4">
-                                        <Header.Content>
-                                            <Header.Subheader
-                                                className={typeClass}
+class LeftImageCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            buttonState: false,
+        };
+        this.changeButtonState = this.changeButtonState.bind(this);
+    }
+
+    changeButtonState() {
+        this.setState({
+            buttonState: true,
+        });
+    }
+
+    render() {
+        const {
+            entityName,
+            location,
+            placeholder,
+            typeClass,
+            type,
+            url,
+        } = this.props;
+        const {
+            buttonState,
+        } = this.state;
+        const entityShortName = renderTextByCharacter(entityName, 25);
+        return (
+            <Grid.Column>
+                <Card className="left-img-card" fluid>
+                    <Card.Header>
+                        <Grid>
+                            <Grid.Row>
+                                <Grid.Column width={6}>
+                                    <Image src={placeholder} />
+                                </Grid.Column>
+                                <Grid.Column width={10}>
+                                    <div style={{padding:'1rem 0rem 0.2rem'}}>
+                                        <Header as="h4">
+                                            <Header.Content>
+                                                <Header.Subheader
+                                                    className={typeClass}
+                                                >
+                                                    {type}
+                                                </Header.Subheader>
+                                                {entityShortName}
+                                                <br />
+                                                <span className="location">
+                                                    {location}
+                                                </span>
+                                            </Header.Content>
+                                        </Header>
+                                        <Link className="lnkChange" route={url}>
+                                            <Button
+                                                disabled={buttonState}
+                                                className="btn-small-white-border"
+                                                onClick={this.changeButtonState}
                                             >
-                                                {type}
-                                            </Header.Subheader>
-                                            {entityShortName}
-                                            <br />
-                                            <span className="location">
-                                                {location}
-                                            </span>
-                                        </Header.Content>
-                                    </Header>
-                                    <Link className="lnkChange" route={url}>
-                                        <Button
-                                            disabled={buttonState}
-                                            className="btn-small-white-border"
-                                            onClick={changeButtonState}
-                                        >
-                                            View
-                                        </Button>
-                                    </Link>
-                                </div>
-                                
-                            </Grid.Column>
-                        </Grid.Row>
-                    </Grid>
-                </Card.Header>
-            </Card>
-        </Grid.Column>
-    );
-};
+                                                View
+                                            </Button>
+                                        </Link>
+                                    </div>
+                                </Grid.Column>
+                            </Grid.Row>
+                        </Grid>
+                    </Card.Header>
+                </Card>
+            </Grid.Column>
+        );
+    }
+}
 
 LeftImageCard.propTypes = {
-    buttonState: PropTypes.bool,
-    changeButtonState: PropTypes.func,
     entityName: PropTypes.string,
     location: PropTypes.string,
     placeholder: PropTypes.string,
@@ -81,8 +95,6 @@ LeftImageCard.propTypes = {
 };
 
 LeftImageCard.defaultProps = {
-    buttonState: false,
-    changeButtonState: _.noop,
     entityName: '',
     location: '',
     placeholder: '',
