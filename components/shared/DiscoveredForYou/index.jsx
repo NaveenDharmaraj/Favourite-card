94849be338a91009b4bc6b0e16506180b424919c
tsfahmed <tousif.ahmed@nirman.io>
2020-03-06 19:58:03 +0530

    No data state text change

--- components/shared/DiscoveredForYou/index.jsx
+++ components/shared/DiscoveredForYou/index.jsx
@@ -14,9 +14,9 @@ const DiscoveredForYou = () => {
         <div className="Discovered">
             <Container>
                 <div className="Discovered-box">
-                    <Header as="h4" className="mb-2">Select causes you care about to see charities and Giving Groups that might interest you.</Header>
+                    <Header as="h4" className="mb-2">Select causes and topics to see charities and Giving Groups that might interest you.</Header>
                     <Link route={'/user/profile/charitableinterest'}>
-                        <Button className="blue-bordr-btn-round-def">Select your causes</Button>
+                        <Button className="blue-bordr-btn-round-def">Select causes and topics</Button>
                     </Link>
                     <div className="dots" />
                     <div className="img_Discovered" />
