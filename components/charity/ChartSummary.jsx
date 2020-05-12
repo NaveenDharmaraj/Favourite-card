f7c5a254ac0960d66bf4367a7e9621f5d379b122
tsfahmed <tousif.ahmed@nirman.io>
2020-05-12 13:34:38 +0530

    chart summary component changes

--- components/charity/ChartSummary.jsx
+++ components/charity/ChartSummary.jsx
@@ -1,4 +1,9 @@
 import React, { Fragment } from 'react';
+import {
+    List,
+    Grid,
+    Header,
+} from 'semantic-ui-react';
 
 const ChartSummary = (props) => {
     const {
@@ -8,9 +13,26 @@ const ChartSummary = (props) => {
     } = props;
     return (
         <Fragment>
-            <p>{color}</p>
-            <p>{text}</p>
-            <p>{value}</p>
+            <Grid.Row className="expenseRow ch_Expenses">
+                <Grid.Column mobile={11} tablet={12} computer={12}>
+                    <List>
+                        <List.Item as="h5">
+                            <div className="boxes">
+                                <div className="boxVLine" />
+                                <div className="box" style={{ backgroundColor: color }} />
+                            </div>
+                            <List.Content>
+                                {text}
+                            </List.Content>
+                        </List.Item>
+                    </List>
+                </Grid.Column>
+                <Grid.Column mobile={5} tablet={4} computer={4} textAlign="right">
+                    <Header as="h5">
+                        {value}
+                    </Header>
+                </Grid.Column>
+            </Grid.Row>
         </Fragment>
     );
 };
