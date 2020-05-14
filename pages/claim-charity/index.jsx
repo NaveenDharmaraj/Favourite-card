558e3793309a2348387be1f923c9ce5dc5a21a4f
preetinair93 <preeti.nair@nirman.io>
2020-05-14 17:21:41 +0530

    Removed laststep and updated to functional comp

--- pages/claim-charity/index.jsx
+++ pages/claim-charity/index.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 import ClaimCharityWrapper from '../../components/ClaimCharity/ClaimCharity';
 import Layout from '../../components/shared/Layout';
 
-class ClaimCharity extends React.Component {
-    render() {
-        return (
-            <Layout authRequired={true}>
-                <ClaimCharityWrapper></ClaimCharityWrapper>
-            </Layout>
-        )
-    }
+function ClaimCharity() {
+    return (
+        <Layout authRequired={true}>
+            <ClaimCharityWrapper></ClaimCharityWrapper>
+        </Layout>
+    )
 }
 
 export default ClaimCharity;
