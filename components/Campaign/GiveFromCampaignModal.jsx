c9ceaa7b7702f78eccde9d2e437939e9eeda48e7
AKHIL P MURALI <itsakhilpm@gmail.com>
2020-05-20 19:39:01 +0530

    removing hardcoded url

--- components/Campaign/GiveFromCampaignModal.jsx
+++ components/Campaign/GiveFromCampaignModal.jsx
@@ -27,8 +27,7 @@ const GiveFromCampaignModal = (props) => {
         campaignRelatedBeneficiariesCount,
         fundId,
     } = props;
-    const relatedBeneficiaryRoute = (campaignRelatedBeneficiariesCount) ? (`/give/to/charity/new?campaign_id=12&source_account_holder_id=448`) : '';
-    // URL getting redirected to dashboard
+    const relatedBeneficiaryRoute = (campaignRelatedBeneficiariesCount) ? (`/give/to/charity/new?campaign_id=${campaignId}&source_account_holder_id=${fundId}`) : '';
     const relatedGroupsRoute = (groupsWithMemberships && groupsWithMemberships.data && groupsWithMemberships.data.length > 0) ? (`/give/to/group/new?source_account_holder_id=${fundId}`) : '';
     return (
         <Modal
