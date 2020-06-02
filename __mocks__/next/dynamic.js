0d8ac63c0ab79d9f6a9b27e1d17c6224299ade25
Aravinth-0812 <aravinth.periasamy@chimpfund.com>
2020-06-02 12:06:03 +0530

    Added basic jest setup to develop

---
+++ __mocks__/next/dynamic.js
@@ -0,0 +1,16 @@
+/* ./__mocks__/next/dynamic.js */
+
+import React from 'react';
+
+const mockReact = React;
+
+let componentName = 'DynamicComponent';
+
+export const __setComponentName = (data) => {
+  componentName = data;
+};
+
+const DynamicComponent = () => ({ children, ...rest }) =>
+  mockReact.createElement(componentName, rest, children);
+
+export default DynamicComponent;
