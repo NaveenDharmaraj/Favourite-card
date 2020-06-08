9920d90baeb66eb463cb48942173e2461e735ac4
tsfahmed <tousif.ahmed@nirman.io>
2020-06-08 18:32:33 +0530

    removed unit test change

--- setupTests.js
+++ setupTests.js
@@ -2,7 +2,3 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
-
-jest.mock('react-chartjs-2', () => ({
-    Bar: () => null, // add any additional chart types here
-}));
