507311877e91f920bf833c1951ef902d24876d2d
preetinair93 <preeti.nair@nirman.io>
2020-07-15 16:23:01 +0530

    Changed class component to functional comp

--- pages/claim-charity/index.jsx
+++ pages/claim-charity/index.jsx
@@ -2,17 +2,29 @@ import React from 'react';
 import ClaimCharityWrapper from '../../components/ClaimCharity/ClaimCharity';
 import Layout from '../../components/shared/Layout';
 
-class ClaimCharity extends React.Component {
-    render() {
-        return (
-            <div>
-                <Layout>
-                    <ClaimCharityWrapper {...this.props} />
-                </Layout>
-            </div>
-        )
-    }
+function ClaimCharity() {
+    return (
+        <div>
+            <Layout>
+                <ClaimCharityWrapper/>
+            </Layout>
+        </div>
+    )
 }
 
-
 export default ClaimCharity;
+
+// class ClaimCharity extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 <Layout>
+//                     <ClaimCharityWrapper {...this.props} />
+//                 </Layout>
+//             </div>
+//         )
+//     }
+// }
+
+
+// export default ClaimCharity;
