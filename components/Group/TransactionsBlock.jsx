01b7778b96883cff22d7cb1eec042de6c45c502c
tsfahmed <tousif.ahmed@nirman.io>
2020-08-07 16:12:50 +0530

    added new transaction section

---
+++ components/Group/TransactionsBlock.jsx
@@ -0,0 +1,104 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import {
+    Header,
+} from 'semantic-ui-react';
+import {
+    number,
+    string,
+} from 'prop-types';
+
+import {
+    formatCurrency,
+} from '../../helpers/give/utils';
+
+import TransactionsCard from './TransactionsCard';
+
+const TransactionsBlock = (props) => {
+    const {
+        groupDetails: {
+            attributes: {
+                totalMoneyRaised,
+                totalMoneyGiven,
+                balance,
+                fundraisingDaysRemaining,
+            },
+        },
+    } = props;
+    const currency = 'USD';
+    const language = 'en';
+    formatCurrency(balance, language, currency);
+    const transactionMapping = [
+        {
+            amount: formatCurrency(totalMoneyRaised, language, currency),
+            field: 'totalMoneyRaised',
+            headerText: 'All time total raised',
+            popupText: 'This is the total money raised since the giving group was created, including past giving goals.',
+        },
+        {
+            amount: formatCurrency(totalMoneyGiven, language, currency),
+            field: 'totalMoneyGiven',
+            headerText: 'Total given',
+            popupText: 'This is the total given to others (eg.: giving groups, campaigns and charities.',
+        },
+        {
+            amount: formatCurrency(balance, language, currency),
+            field: 'balance',
+            headerText: 'Total balance',
+            popupText: 'This is how much the group currently have. All time total raised minus total given to others.',
+        },
+    ];
+    const transactionList = transactionMapping.map((transaction) => {
+        if (!(transaction.field === 'totalMoneyRaised' && fundraisingDaysRemaining === 0)) {
+            return (
+                <TransactionsCard
+                    transactionDetails={transaction}
+                />
+            );
+        }
+        return null;
+    });
+
+    return (
+        <div className="charityInfowrap fullwidth">
+            <div className="charityInfo">
+                <Header as="h4">Transactions</Header>
+                {transactionList}
+                <div className="lastGiftWapper">
+                    <p className="lastGiftText blueText">View transactions</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+TransactionsBlock.defaultProps = {
+    groupDetails: {
+        attributes: {
+            balance: '',
+            fundraisingDaysRemaining: null,
+            totalMoneyGiven: '',
+            totalMoneyRaised: '',
+        },
+    },
+};
+
+TransactionsBlock.propTypes = {
+    groupDetails: {
+        attributes: {
+            balance: string,
+            fundraisingDaysRemaining: number,
+            totalMoneyGiven: string,
+            totalMoneyRaised: string,
+        },
+    },
+};
+
+function mapStateToProps(state) {
+    return {
+        groupDetails: state.group.groupDetails,
+        isAuthenticated: state.auth.isAuthenticated,
+    };
+}
+
+export default connect(mapStateToProps)(TransactionsBlock);
---
+++ components/Group/TransactionsCard.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import {
+    Header,
+    Popup,
+    Icon,
+} from 'semantic-ui-react';
+import {
+    string,
+} from 'prop-types';
+
+const TransactionsCard = (props) => {
+    const {
+        transactionDetails: {
+            amount,
+            headerText,
+            popupText,
+            field,
+        },
+    } = props;
+    return (
+        <div className="boxGroup">
+            <div className="Currentbox">
+                <Header as="h3" className={`${(field === 'balance') ? 'green' : ''}`}>{amount}</Header>
+                <p>{headerText}</p>
+            </div>
+            <div className="Currentboxpop">
+                <Popup
+                    trigger={(
+                        <Icon
+                            name="question circle"
+                        />
+                    )}
+                    content={popupText}
+                    position="top right"
+                    inverted
+                />
+            </div>
+        </div>
+    );
+};
+
+TransactionsCard.defaultProps = {
+    transactionDetails: {
+        amount: '',
+        field: '',
+        headerText: '',
+        popupText: '',
+    },
+};
+
+TransactionsCard.propTypes = {
+    transactionDetails: {
+        amount: string,
+        field: string,
+        headerText: string,
+        popupText: string,
+    },
+};
+
+export default TransactionsCard;
