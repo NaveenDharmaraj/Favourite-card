bb019e13ddeb4c2ce1d49369f270d2c8a09ae5a9
tsfahmed <tousif.ahmed@nirman.io>
2020-08-07 14:44:17 +0530

    removed code to handle thankyou scenario

--- components/shared/ActiveMatchBlock/index.jsx
+++ components/shared/ActiveMatchBlock/index.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import {
     Grid,
     Button,
@@ -12,9 +12,6 @@ import {
 import placeHolderImage from '../../../static/images/no-data-avatar-giving-group-profile.png';
 
 const ActiveMatchBlock = (props) => {
-    // const {
-    //     entityDetails,
-    // } = props;
     const {
         activeMatch: {
             balance,
@@ -32,75 +29,37 @@ const ActiveMatchBlock = (props) => {
     const formattedBalance = formatCurrency(balance, language, currency);
     const formattedmaxMatchAmount = formatCurrency(maxMatchAmount, language, currency);
     const formattedtotalMatch = formatCurrency(totalMatch, language, currency);
-    const matchExpired = (balance === '0');
+    const matchExpired = (balance === '0'); // TODO check matching expired or not
     const headingText = matchExpired ? 'Thank you for your support!' : 'Your gift will be matched!';
     return (
-        <div className="charityInfowrap tabcharityInfowrap fullwidth lightGreenBg">
+        <div className="charityInfowrap fullwidth lightGreenBg">
             <div className="charityInfo">
                 <Header as="h4">{headingText}</Header>
-                {matchExpired
-                    ? (
-                        <Fragment>
-                            <p>
-                                    Between November 1, 2019 and
-                                    &nbsp;
-                                {matchClose}
-                                ,
-                                <b>{company}</b>
-                                &nbsp;
-                                    generously matched each gift to this group dollar for dollar.
-                            </p>
-                            <div className="matchingFundsText">
-                                <Header as="h3">{formattedBalance}</Header>
-                                {/* <Header as="h5"> was matched by Charitable Impact</Header>
-                                <div className="total">
-                                    <p>
-                                        of
-                                        {formattedtotalMatch}
-                                        &nbsp;
-                                    provided by
-                                        {company}
-                                    </p>
-                                </div> */}
+                <p>
+                    For every $1.00 you give to this group,
+                    <b>{company}</b>
+                    &nbsp;will match your gift with $1.00 up to&nbsp;
+                    <b>{formattedmaxMatchAmount}</b>
+                    &nbsp;per gift.
+                </p>
+                <div className="matchingFundsWapper">
+                    <div className="matchingFundsGraff">
+                        <div className="Progresswapper">
+                            <div className="customProgress">
+                                <div className="bar" style={{ height: `${matchPercent}%` }} />
                             </div>
-                        </Fragment>
-                    )
-                    : (
-                        <Fragment>
+                        </div>
+                    </div>
+                    <div className="matchingFundsText">
+                        <Header as="h3">{formattedBalance}</Header>
+                        <Header as="h5"> matching funds remaining</Header>
+                        <div className="total">
                             <p>
-                            For every $1.00 you give to this group,
-                                <b>{company}</b>
-                                &nbsp;
-                            will match your gift with $1.00 up to
-                                &nbsp;
-                                <b>{formattedmaxMatchAmount}</b>
-                                per gift.
+                                {` of ${formattedtotalMatch} provided by ${company}`}
                             </p>
-                            <div className="matchingFundsWapper">
-                                <div className="matchingFundsGraff">
-                                    <div className="Progresswapper">
-                                        <div className="customProgress">
-                                            <div className="bar" style={{ height: `${matchPercent}%` }} />
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="matchingFundsText">
-                                    <Header as="h3">{formattedBalance}</Header>
-                                    <Header as="h5"> matching funds remaining</Header>
-                                    <div className="total">
-                                        <p>
-                                            of
-                                            {formattedtotalMatch}
-                                            &nbsp;
-                                        provided by
-                                            {company}
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                        </Fragment>
-                    )
-                }
+                        </div>
+                    </div>
+                </div>
                 <Grid.Row className="MatchingPartnerWapper">
                     <Grid>
                         <Grid.Column mobile={3} tablet={4} computer={4} className="pr-0">
@@ -117,9 +76,7 @@ const ActiveMatchBlock = (props) => {
                     </Grid>
                 </Grid.Row>
                 <Button className="white-btn-rounded-def goalbtn">
-                    Expires
-                    &nbsp;
-                    {matchClose}
+                    {` Expires ${matchClose}`}
                 </Button>
                 {((type === 'groups') && !matchExpired)
                 && (
@@ -127,41 +84,6 @@ const ActiveMatchBlock = (props) => {
                 )}
             </div>
         </div>
-        // <div className="active-match-message mt-3">
-        //     <Message>
-        //         <Grid>
-        //             <Grid.Row>
-        //                 <Grid.Column computer={13} tablet={12} mobile={8}>
-        //                     <div className="active-match-heading mb-0 pb-0">
-        //                         <h2 class="mb-0 pb-0">Your gift will be matched!</h2>
-        //                     </div>
-        //                 </Grid.Column>
-        //                 {(entityDetails.attributes.activeMatch.matchClose) ?
-        //                     (
-        //                         <Grid.Column computer={3} tablet={4} mobile={8}>
-        //                             <div className="active-match-btn">
-        //                                 <div className="btn-active-match">
-        //                                     {`Expires ${entityDetails.attributes.activeMatch.matchClose}`}
-        //                                 </div>
-        //                             </div>
-        //                         </Grid.Column>
-        //                     ) : null}
-        //             </Grid.Row>
-        //             <Grid.Row>
-        //                 <Grid.Column computer={13} tablet={12} mobile={16}> 
-        //                     <div className="active-match-comments">
-        //                         <p>
-        //                             {entityDetails.attributes.activeMatch.company} will match 100% for each $1.00 you give to this group, until matching funds run out or expire.
-        //                         </p>
-        //                         <p><span className="active-match-dollar"><b>{formattedBalance}</b></span> matching funds left.</p>
-        //                     </div>
-        //                 </Grid.Column>
-        //                 <Grid.Column computer={3} tablet={4} mobile={0}>
-        //                 </Grid.Column>
-        //             </Grid.Row>
-        //         </Grid>
-        //     </Message>
-        // </div>
     );
 };
 export default ActiveMatchBlock;
