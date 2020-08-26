69f9a66e42cf295c7d0f6ebb35d39607e528f92c
preetinair93 <preeti.nair@nirman.io>
2020-08-26 11:12:03 +0530

    Reducer unit testing.

---
+++ __tests__/reducers/Data/profile_Data.js
@@ -0,0 +1,222 @@
+export const campaignSubGroupDetails = {
+    data: [{
+        attributes: {
+            about: "",
+            activeMatch: {},
+            automaticMatching: false,
+            avatar: "https://d3guc82o1rxzlm.cloudfront.net/groups/logos/999031/display/data.jpeg?1597399237",
+            balance: "55.00",
+            campaignAvatar: "https://d3guc82o1rxzlm.cloudfront.net/groups/logos/53/display/data.jpeg?1597217117",
+            campaignCity: null,
+            campaignId: 12,
+            campaignName: "Animal Action",
+            campaignSlug: "animal-action",
+            causes: [],
+            city: "Vancouver",
+            createdAt: "2020-08-14T03:00:16.000-07:00",
+            currentUserIsAdmin: true,
+            description: "GiveItUpForPeace",
+            following: false,
+            formattedAbout: null,
+            formattedHelping: "By counselling.",
+            formattedImpact: "For Peace",
+            formattedShort: "GiveItUpForPeace",
+            fundId: 781,
+            fundName: "Birds squad's Impact Account",
+            fundraisingDaysRemaining: 0,
+            fundraisingEndDate: "2020-08-13",
+            fundraisingPercentage: 4,
+            fundraisingStartDate: "2020-08-14T03:01:18.000-07:00",
+            goal: "1227.0",
+            goalAmountRaised: "55.0",
+            groupType: "Automotive",
+            hasActiveMatch: false,
+            hasCampaignAccess: true,
+            hasLocation: true,
+            helping: "By counselling.",
+            isAdmin: true,
+            isCampaign: false,
+            isCampaignPrivate: false,
+            isMember: true,
+            isPrivate: false,
+            lastDonationAt: "2020-08-19T04:54:46.000-07:00",
+            liked: false,
+            likesCount: 0,
+            location: "Vancouver, BC",
+            memberCount: 1,
+            mobileDescription: "GiveItUpForPeace",
+            name: "Birds squad",
+            organizer: null,
+            province: "BC",
+            purpose: "For Peace",
+            recurringEnabled: false,
+            shortDescription: "GiveItUpForPeace",
+            shortUrl: "https://my.dev.charitableimpact.com/groups/birds-squad?utm_campaign=groups&utm_medium=social&utm_source=direct&utm_term=Birds+squad",
+            slug: "birds-squad",
+            tags: [],
+            totalMoneyGiven: "0.0",
+            totalMoneyRaised: "55.0",
+            videoDirectLink: "http://www.youtube.com/v/YTJg8q9Q940?version=3&autohide=1",
+            videoPlayerLink: "https://www.youtube.com/embed/YTJg8q9Q940?controls=0&modestbranding=1&showinfo=0&rel=0",
+        },
+        id: "21",
+        links: {
+            self: "https://api.dev.charitableimpact.com/core/v2/groups/999031",
+            next: "https://api.dev.charitableimpact.com/core/v2/groups/999031",
+        },
+        relationships: {
+            activities: {
+                links: {
+                    related: "https://api.dev.charitableimpact.com/core/v2/groups/999031/activities",
+                    self: "https://api.dev.charitableimpact.com/core/v2/groups/999031/relationships/activities",
+                }
+            },
+            campaign: {
+                links: {
+                    related: "https://api.dev.charitableimpact.com/core/v2/groups/999031/campaign",
+                    self: "https://api.dev.charitableimpact.com/core/v2/groups/999031/relationships/campaign",
+                }
+            },
+            fund: {
+                links: {
+                    related: "https://api.dev.charitableimpact.com/core/v2/groups/999031/fund",
+                    self: "https://api.dev.charitableimpact.com/core/v2/groups/999031/relationships/fund",
+                }
+            },
+            galleryImages: {
+                links: {
+                    related: "https://api.dev.charitableimpact.com/core/v2/groups/999031/galleryImages",
+                    self: "https://api.dev.charitableimpact.com/core/v2/groups/999031/relationships/galleryImages",
+                }
+            },
+            groupAdmins: {
+                links: {
+                    related: "https://api.dev.charitableimpact.com/core/v2/groups/999031/groupAdmins",
+                    self: "https://api.dev.charitableimpact.com/core/v2/groups/999031/relationships/groupAdmins",
+                }
+            },
+            groupBeneficiaries: {
+                links: {
+                    related: "https://api.dev.charitableimpact.com/core/v2/groups/999031/groupBeneficiaries",
+                    self: "https://api.dev.charitableimpact.com/core/v2/groups/999031/relationships/groupBeneficiaries",
+                }
+            },
+            groupMembers: {
+                links: {
+                    related: "https://api.dev.charitableimpact.com/core/v2/groups/999031/groupMembers",
+                    self: "https://api.dev.charitableimpact.com/core/v2/groups/999031/relationships/groupMembers",
+                }
+            },
+            groupUsers: {
+                links: {
+                    related: "https://api.dev.charitableimpact.com/core/v2/groups/999031/groupUsers",
+                    self: "https://api.dev.charitableimpact.com/core/v2/groups/999031/relationships/groupUsers",
+                }
+            },
+            onlyGroupMembers: {
+                links: {
+                    related: "https://api.dev.charitableimpact.com/core/v2/groups/999031/onlyGroupMembers",
+                    self: "https://api.dev.charitableimpact.com/core/v2/groups/999031/relationships/onlyGroupMembers",
+                }
+            },
+        },
+        type: "groups",
+    }],
+    links: {
+        first: "https://api.dev.charitableimpact.com/core/v2/campaigns/12/subGroups?page%5Bnumber%5D=1&page%5Bsize%5D=6",
+        last: "https://api.dev.charitableimpact.com/core/v2/campaigns/12/subGroups?page%5Bnumber%5D=3&page%5Bsize%5D=6",
+        next: "https://api.dev.charitableimpact.com/core/v2/campaigns/12/subGroups?page%5Bnumber%5D=2&page%5Bsize%5D=6",
+    },
+    meta: {
+        pageCount: 3,
+        recordCount: 17,
+    }
+};
+
+export const campaignDetails = {
+    attributes: {
+        about: "",
+        activeMatch: {},
+        amountRaised: "3034.0",
+        automaticMatching: false,
+        avatar: "https://d3guc82o1rxzlm.cloudfront.net/groups/logos/999031/display/data.jpeg?1597399237",
+        balance: "55.00",
+        banner: "",
+        causes: [],
+        city: "Vancouver",
+        createdAt: "2020-08-14T03:00:16.000-07:00",
+        following: false,
+        formattedImpact: "For Peace",
+        formattedShort: "GiveItUpForPeace",
+        fundId: 781,
+        fundName: "Birds squad's Impact Account",
+        groupId: 50,
+        groupsCount: 0,
+        hasActiveMatch: false,
+        helping: null,
+        impact: "Ullam ea fugiat sed non id.",
+        isAdmin: true,
+        liked: false,
+        mobileDescription: "GiveItUpForPeace",
+        name: "Birds squad",
+        peopleInCampaign: 4,
+        province: null,
+        recurringEnabled: false,
+        short: "Ab perspiciatis iusto et eos.",
+        slug: "clean-water-for-all",
+        tags: [],
+        videoDirectLink: '',
+        videoPlayerLink: '',
+    },
+    id: "21",
+    links: {
+        self: "https://api.dev.charitableimpact.com/core/v2/groups/999031",
+    },
+    relationships: {
+        galleryImages: {
+            links: {
+                related: "https://api.dev.charitableimpact.com/core/v2/groups/999031/galleryImages",
+                self: "https://api.dev.charitableimpact.com/core/v2/groups/999031/relationships/galleryImages",
+            }
+        },
+        groupBeneficiaries: {
+            links: {
+                related: "https://api.dev.charitableimpact.com/core/v2/groups/999031/groupBeneficiaries",
+                self: "https://api.dev.charitableimpact.com/core/v2/groups/999031/relationships/groupBeneficiaries",
+            }
+        },
+        subGroups: {
+            links: {
+                related: "https://api.dev.charitableimpact.com/core/v2/groups/999031/onlyGroupMembers",
+                self: "https://api.dev.charitableimpact.com/core/v2/groups/999031/relationships/onlyGroupMembers",
+            }
+        },
+    },
+    type: "campaigns",
+}
+
+export const campaignImageGallery = [
+    {
+        attributes: {
+            assetContentType: 'image/jpeg',
+            assetFileName: 'baby3.jpeg',
+            assetFileSize: 5713,
+            assetUrl: 'https://d3guc82o1rxzlm.cloudfront.net/assets/assets/55/display/baby3.jpeg?1597217367',
+            originalUrl: 'https://d3guc82o1rxzlm.cloudfront.net/assets/assets/55/original/baby3.jpeg?1597217367',
+        },
+        id: '55',
+        links: {
+            self: 'https://api.dev.charitableimpact.com/core/v2/assets/55',
+        },
+        type: 'assets',
+    }
+];
+
+export const deepLink = {
+    attributes: {
+        previewlink: "https://m.24467.org/wvdj?d=1",
+        shortlink: "https://m.24467.org/wvdj",
+    },
+    id: "1598250482713:ip-10-20-1-107.ec2.internal:22:ke4by39d:10231",
+    type: "deeplinks",
+}
---
+++ __tests__/reducers/profile.test.js
@@ -0,0 +1,158 @@
+
+import profile from '../../reducers/profile';
+
+import { campaignDetails, campaignSubGroupDetails, deepLink, campaignImageGallery } from './Data/profile_Data';
+
+describe('Testing campaign profile Reducer', () => {
+    it('Should match state with payload for campaign data', () => {
+        const type = 'GET_CAMPAIGN_FROM_SLUG';
+        const payload = {
+            campaignDetails,
+        };
+        const newState = profile(undefined, {
+            payload,
+            type,
+        });
+        expect(newState).toEqual(payload);
+    });
+
+    it('Should reset campaign subgroup data', () => {
+        const type = 'CLEAR_DATA_FOR_CAMPAIGNS';
+        const payload = {
+            campaignSubGroupDetails,
+            campaignSubGroupsShowMoreUrl: null,
+        };
+        const newState = profile(undefined, {
+            payload,
+            type,
+        });
+        expect(newState).toEqual(payload);
+    });
+
+    it('Should match state with expected for campaign subgroup data', () => {
+        const type = 'GET_SUB_GROUPS_FOR_CAMPAIGN';
+        const payload = {
+            campaignSubGroupDetails,
+        };
+        const expected = {
+            campaignSubGroupDetails: campaignSubGroupDetails.data,
+            campaignSubGroupsShowMoreUrl: campaignSubGroupDetails.links.next,
+        }
+        const newState = profile({}, {
+            payload,
+            type,
+        });
+        expect(newState).toEqual(expected);
+    });
+
+    it('Should match state with payload for campaign subgroup loader', () => {
+        const type = 'SUB_GROUP_LIST_LOADER';
+        const payload = {
+            subGroupListLoader: true,
+        };
+        const newState = profile(undefined, {
+            payload,
+            type,
+        });
+        expect(newState).toEqual(payload);
+    });
+
+    it('Should match state with payload for deeplink url', () => {
+        const type = 'DEEP_LINK_URL';
+        const payload = {
+            deepLink,
+        };
+        const expected = {
+            deepLinkUrl: {
+                ...deepLink,
+            },
+        }
+        const newState = profile(undefined, {
+            payload,
+            type,
+        });
+        expect(newState).toEqual(expected);
+    });
+
+    it('Should match state with payload for disable follow button', () => {
+        const type = 'DISABLE_FOLLOW_BUTTON';
+        const payload = {
+            disableFollow: false,
+        };
+        const newState = profile(undefined, {
+            payload,
+            type,
+        });
+        expect(newState).toEqual(payload);
+    });
+
+    it('Should match state with payload for see more loader', () => {
+        const type = 'SEE_MORE_LOADER';
+        const payload = {
+            seeMoreLoaderStatus: true,
+        };
+        const newState = profile(undefined, {
+            payload,
+            type,
+        });
+        expect(newState).toEqual(payload);
+    });
+
+    it('Should match state with payload for getting images for campaign', () => {
+        const type = 'GET_IMAGES_FOR_CAMPAIGN';
+        const payload = {
+            campaignImageGallery,
+        };
+        const newState = profile(undefined, {
+            payload,
+            type,
+        });
+        expect(newState).toEqual(payload);
+    });
+
+    it('Should match state with payload for ', () => {
+        const type = 'SAVE_FOLLOW_STATUS_CAMPAIGN';
+        const payload = {
+            followStatus: true,
+        };
+        const expected = {
+            campaignDetails: {
+                ...campaignDetails,
+                attributes: {
+                    ...campaignDetails.attributes,
+                    liked: payload.followStatus,
+                },
+            },
+        }
+        const newState = profile({ campaignDetails }, {
+            payload,
+            type,
+        });
+        expect(newState).toEqual(expected);
+    });
+
+    it('Should match state with payload for api error status', () => {
+        const type = 'SLUG_API_ERROR_STATUS';
+        const payload = {
+            slugApiErrorStats: false,
+        };
+        const newState = profile(undefined, {
+            payload,
+            type,
+        });
+        expect(newState).toEqual(payload);
+    });
+
+    it('Should match state with payload for storing search key data', () => {
+        const type = 'STORE_SEARCH_KEY_FOR_CAMPAIGN';
+        const payload = {
+            searchData: 'abc',
+        };
+        const newState = profile(undefined, {
+            payload,
+            type,
+        });
+        expect(newState).toEqual(payload);
+    });
+
+})
