29f09f53279257dc5aaa1f7e4b16eddd95b415c0
Aravinth-0812 <aravinth.periasamy@chimpfund.com>
2020-09-15 16:37:57 +0530

    removed comments

--- Firebase/config.js
+++ Firebase/config.js
@@ -4,13 +4,13 @@ const firebaseConfig = {
     },
     set firebaseConfigGetSet(FIREBASE_PARAMS = {}) {
         this.firebaseEnvs = {
-            apiKey: FIREBASE_PARAMS[`${this.firebaseEnvKeys.FIREBASE_API_KEY}`], // "AIzaSyCVmEnXltzSaZb38zzjSsAu7_T8LG5jxho",
-            authDomain: FIREBASE_PARAMS[`${this.firebaseEnvKeys.FIREBASE_AUTH_DOMAIN}`], // "chimp-fbmobile-dev.firebaseapp.com",
-            databaseURL: FIREBASE_PARAMS[`${this.firebaseEnvKeys.FIREBASE_DATABASE_URL}`], // "https://chimp-fbmobile-dev.firebaseio.com",
-            projectId: FIREBASE_PARAMS[`${this.firebaseEnvKeys.FIREBASE_PROJECT_ID}`], // "chimp-fbmobile-dev",
-            storageBucket: FIREBASE_PARAMS[`${this.firebaseEnvKeys.FIREBASE_STORAGE_BUCKET}`], // "chimp-fbmobile-dev.appspot.com",
-            messagingSenderId: FIREBASE_PARAMS[`${this.firebaseEnvKeys.FIREBASE_MSG_SENDER_ID}`] || "745841700240",
-            appId: FIREBASE_PARAMS[`${this.firebaseEnvKeys.FIREBASE_APP_ID}`], // "1:745841700240:web:7f010837ea13d384"
+            apiKey: FIREBASE_PARAMS[`${this.firebaseEnvKeys.FIREBASE_API_KEY}`],
+            authDomain: FIREBASE_PARAMS[`${this.firebaseEnvKeys.FIREBASE_AUTH_DOMAIN}`],
+            databaseURL: FIREBASE_PARAMS[`${this.firebaseEnvKeys.FIREBASE_DATABASE_URL}`],
+            projectId: FIREBASE_PARAMS[`${this.firebaseEnvKeys.FIREBASE_PROJECT_ID}`],
+            storageBucket: FIREBASE_PARAMS[`${this.firebaseEnvKeys.FIREBASE_STORAGE_BUCKET}`],
+            messagingSenderId: FIREBASE_PARAMS[`${this.firebaseEnvKeys.FIREBASE_MSG_SENDER_ID}`] || '745841700240',
+            appId: FIREBASE_PARAMS[`${this.firebaseEnvKeys.FIREBASE_APP_ID}`],
         };
     },
     firebaseEnvKeys: {
