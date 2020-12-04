fb8c47651d91400790b978f2b077c82a709b34f6
tsfahmed <tsfahmed@users.noreply.github.com>
2020-12-04 11:03:38 +0530

    Task/user profile remove unused code (#1658)
    * removed unused component import
    
    * removed unused components
    
    Co-authored-by: tsfahmed <tousif.ahmed@nirman.io>

--- pages/myProfile.jsx
+++ pages/myProfile.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import _ from 'lodash';
+import _isEmpty from 'lodash/isEmpty';
 import {
     Container,
-    Tab,
     Header,
 } from 'semantic-ui-react';
 import {
@@ -14,16 +13,9 @@ import {
     getUserProfileBasic,
 } from '../actions/userProfile';
 import Layout from '../components/shared/Layout';
-import BasicProfile from '../components/MyProfile/BasicProfile';
-import EditProfileBasic from '../components/MyProfile/EditBasicProfile';
-import EditCharitableInterest from '../components/MyProfile/EditCharitableInterest';
-const Friends = dynamic(() => import('../components/MyProfile/Friends'), {
-    ssr: false
-});
 const Settings = dynamic(() => import('../components/MyProfile/Settings'), {
     ssr: false
 });
-import { Router } from '../routes';
 
 class MyProfile extends React.Component {
     static async getInitialProps({ query }) {
@@ -36,23 +28,12 @@ class MyProfile extends React.Component {
         };
     }
 
-    constructor(props) {
-        super(props);
-        const {
-            pageName,
-        } = this.props;
-        const activeTabIndex = _.isEmpty(pageName) ? 0 : this.getPageIndexByName(pageName);
-        this.state = {
-            activeTabIndex,
-        };
-    }
-
     componentDidMount() {
         const {
             currentUser,
             dispatch,
         } = this.props;
-        if (!_.isEmpty(currentUser)) {
+        if (!_isEmpty(currentUser)) {
             const {
                 id,
                 attributes: {
@@ -63,39 +44,7 @@ class MyProfile extends React.Component {
         }
     }
 
-    getPageIndexByName(pageName) {
-        switch (pageName) {
-            case 'basic':
-                return 0;
-            case 'charitableinterest':
-                return 1;
-            case 'friends':
-                return 2
-            case 'settings':
-                return 3
-            default:
-                break;
-        }
-    }
-
     render() {
-        const {
-            userProfileBasicData,
-            currentUser,
-        } = this.props;
-        const {
-            activeTabIndex,
-        } = this.state;
-        let userData = '';
-        if (userProfileBasicData
-            && userProfileBasicData.data
-            && _.size(userProfileBasicData.data) > 0) {
-            userData = userProfileBasicData.data[0].attributes;
-        }
-        let userAvatar = '';
-        if (!_.isEmpty(currentUser)) {
-            userAvatar = currentUser.attributes.avatar;
-        }
         return (
             <Layout authRequired stripe>
                 <Container>
@@ -112,8 +61,6 @@ class MyProfile extends React.Component {
 function mapStateToProps(state) {
     return {
         currentUser: state.user.info,
-        userProfileBasicData: state.userProfile.userProfileBasicData,
-        userCausesList: state.userProfile.userCausesList,
     };
 }
 
