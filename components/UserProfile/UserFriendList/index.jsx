67bb04122e19ac99a1ffc546a08b8de9cbae4c59
murugananthamR11 <webdeveloper.murali@gmail.com>
2020-12-08 16:23:26 +0530

    UI_fixes

--- components/UserProfile/UserFriendList/index.jsx
+++ components/UserProfile/UserFriendList/index.jsx
@@ -584,7 +584,30 @@ class UserFriendList extends React.Component {
                         <Tab.Pane>
                             
                             <div className='findFriendsSearch'>
-                                
+                                {/* <Dropdown
+                                    // options= {friendDropdownList}
+                                    placeholder="Find friends already on Charitable Impact"
+                                    search
+                                    fluid
+                                    selection
+                                    default={false}
+                                    // open={!_isEmpty(friendDropdownList) ? true : false}
+                                    // icon={null}
+                                    // loading={true}
+                                    // icon={<Icon
+                                    //     className='delete'
+                                    //     onClick={this.clearSearch}
+                                    //     />}
+                                    icon={<Icon
+                                        className='search'
+                                        onClick={this.handleFriendSearch}
+                                        />}
+                                    value={friendSearchText}
+                                    onSearchChange={this.handleTypeAheadSearch}
+                                    {...(showDropdownLoader ? ({loading: true}) : undefined)}
+                                    {...(!_isEmpty(friendDropdownList) ? ({open: true}) : ({open: false}))}
+                                    {...(!_isEmpty(friendDropdownList) ? ({options: friendDropdownList}) : undefined)}
+                                /> */}
                                 <Search
                                     fluid
                                     placeholder="Find friends already on Charitable Impact"
