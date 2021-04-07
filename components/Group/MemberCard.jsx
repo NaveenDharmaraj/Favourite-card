cbd925282a6be359d326ae38c55c97106aecc285
Aravinth-0812 <aravinth0812@gmail.com>
2021-04-07 15:16:26 +0530

    added different button for member in group

--- components/Group/MemberCard.jsx
+++ components/Group/MemberCard.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
 import {
     List,
     Image,
     Button,
     Table,
+    Dropdown,
 } from 'semantic-ui-react';
 import _isEmpty from 'lodash/isEmpty';
 import {
@@ -23,11 +24,20 @@ import {
 import {
     addFriendRequest,
 } from '../../actions/group';
+import {
+    acceptFriend,
+    ingnoreFriendRequest,
+} from '../../actions/userProfile';
 
 class MemberCard extends React.Component {
     constructor(props) {
         super(props);
+        let updatedStatus = !_isEmpty(props.memberData.attributes.friendStatus) ? props.memberData.attributes.friendStatus : '';
+        if (props.memberData.attributes.friendIgnoredStatus === "IGNORED" && props.memberData.attributes.friendStatus !== "PENDING_OUT") {
+            updatedStatus = '';
+        }
         this.state = {
+            updatedStatus,
             buttonState: false,
         };
         this.addFriend = this.addFriend.bind(this);
@@ -66,11 +76,65 @@ class MemberCard extends React.Component {
         });
         dispatch(addFriendRequest(user)).then(() => {
             this.setState({
+                updatedStatus: 'PENDING_OUT',
                 buttonState: false,
             });
         });
     }
-
+    rejectInvite(type, rejectType = 'ignore') {
+        const {
+            currentUser: {
+                attributes: {
+                    email,
+                },
+                id: currentUserId,
+            },
+            dispatch,
+            memberData: {
+                id: memberUserId,
+            },
+        } = this.props;
+        dispatch(ingnoreFriendRequest(currentUserId, memberUserId, email, type, rejectType))
+            .then(() => {
+                this.setState({
+                    updatedStatus: ''
+                })
+            })
+            .catch(() => {
+                //handle error
+            });
+    }
+    handleAcceptRequest() {
+        const {
+            currentUser: {
+                id,
+                attributes: {
+                    avatar,
+                    email,
+                    firstName,
+                    displayName,
+                },
+            },
+            memberData: {
+                attributes: {
+                    email: friendEmail,
+                },
+                id: memberUserId,
+            },
+            dispatch,
+        } = this.props;
+        if (!_isEmpty(email)) {
+            acceptFriend(dispatch, id, email, avatar, firstName, Number(memberUserId), friendEmail)
+                .then(() => {
+                    this.setState({
+                        updatedStatus: 'ACCEPTED'
+                    })
+                })
+                .catch(() => {
+                    //handle error
+                })
+        }
+    }
     render() {
         const {
             memberData: {
@@ -91,29 +155,43 @@ class MemberCard extends React.Component {
         } = this.props;
         const {
             buttonState,
+            updatedStatus,
         } = this.state;
-        const statusMapping = {
-            ACCEPTED: formatMessage('groupProfile:messageText'),
-            BLOCKED_OUT: formatMessage('groupProfile:blocked'),
-            default: formatMessage('groupProfile:addFriend'),
-            PENDING_IN: formatMessage('groupProfile:pending'),
-            PENDING_OUT: formatMessage('groupProfile:pending'),
-        };
-        let friendStatusText = '';
+        let buttonText = '';
+        let buttonClass = 'blue-btn-rounded-def';
+        switch (updatedStatus) {
+            case 'ACCEPTED':
+                buttonText = formatMessage('groupProfile:messageText');
+                break;
+            case 'PENDING_IN':
+                buttonText = formatMessage('groupProfile:accept');
+                buttonClass = 'blue-bordr-btn-round-def';
+                break;
+            case 'PENDING_OUT':
+                buttonText = formatMessage('groupProfile:pending');
+                buttonClass = 'blue-bordr-btn-round-def';
+                break;
+            case 'BLOCKED_OUT':
+                buttonText = formatMessage('groupProfile:blocked');
+                buttonClass = 'blue-bordr-btn-round-def';
+                break;
+            default:
+                buttonText = formatMessage('groupProfile:addFriend');
+                buttonClass = 'blue-bordr-btn-round-def';
+                break;
+        }
         const isUserBlocked = (friendStatus && friendStatus === 'BLOCKED_IN');
         const isBlockedMember = (friendStatus && friendStatus === 'BLOCKED_OUT');
-        const isRequestPending = (friendStatus && friendStatus.substring(0, 7) === 'PENDING');
-        const isFriend = (friendStatus && friendStatus === 'ACCEPTED');
         const isCurrentUser = (currentUserId === memberUserId);
         const userDisplayName = isUserBlocked ? formatMessage('groupProfile:anonymousUser') : displayName;
-        const disableButton = (isRequestPending | isBlockedMember);
         const hideButton = (isCurrentUser || isUserBlocked);
-
-        if (!hideButton) {
-            friendStatusText = (_isEmpty(friendStatus)
-                ? statusMapping.default : statusMapping[friendStatus]);
-        }
-
+        const buttonElement = (
+            <Button
+                className={`${buttonClass} c-small`}
+            >
+                {buttonText}
+            </Button>
+        );
         return (
             <Table.Body>
                 <Table.Row className="EmilyData">
@@ -125,11 +203,11 @@ class MemberCard extends React.Component {
                                     <List.Header className="EmilyAdmin">
                                         {`${userDisplayName} ${isGroupAdmin ? `• ${formatMessage('groupProfile:admin')}` : ''}`}
                                         {isGroupAdmin
-                                        && (
-                                            <span>
-                                                <i aria-hidden="true" className="icon star outline" />
-                                            </span>
-                                        )}
+                                            && (
+                                                <span>
+                                                    <i aria-hidden="true" className="icon star outline" />
+                                                </span>
+                                            )}
                                     </List.Header>
                                     <List.Description>
                                         <p>
@@ -141,31 +219,64 @@ class MemberCard extends React.Component {
                         </List>
                     </Table.Cell>
                     <Table.Cell className="amount">
-                        {(!hideButton && !isFriend)
-                                && (
-                                    <Button
-                                        className={`btnFrinend ${(disableButton) ? 'blue-btn-rounded-def' : 'blue-bordr-btn-round-def'}`}
-                                        disabled={disableButton || buttonState}
-                                        onClick={this.addFriend}
-                                    >
-                                        {friendStatusText}
-                                    </Button>
-                                )}
-                        {(!hideButton && isFriend)
-                            && (
-                                <Link route={`/chats/${memberUserId}`}>
-                                    <Button
-                                        className="blue-btn-rounded-def c-small"
-                                    >
-                                        {friendStatusText}
-                                    </Button>
-                                </Link>
-                            )
-
+                        {!hideButton &&
+                            <Fragment>
+                                {(!_isEmpty(updatedStatus) && updatedStatus === 'ACCEPTED')
+                                    && (
+                                        <Link route={`/chats/${memberUserId}`}>
+                                            { buttonElement}
+                                        </Link>
+                                    )}
+                                {(!_isEmpty(updatedStatus) && updatedStatus === 'PENDING_IN')
+                                    && (
+                                        <Fragment>
+                                            <Button
+                                                className={`${buttonClass} c-small`}
+                                                onClick={() => this.handleAcceptRequest()}
+                                            >
+                                                {buttonText}
+                                            </Button>
+                                            <a className='ignore' onClick={() => this.rejectInvite('friends', 'ignore')}>Ignore</a>
+                                        </Fragment>
+                                    )}
+                                {(!_isEmpty(updatedStatus) && updatedStatus === 'PENDING_OUT')
+                                    && (
+                                        <Fragment>
+                                            <Dropdown
+                                                className='userProfile_drpbtn'
+                                                icon='chevron down'
+                                                direction='left'
+                                                trigger={(
+                                                    <Button
+                                                        className="blue-bordr-btn-round-def"
+                                                    >
+                                                        Pending
+                                                    </Button>
+                                                )}
+                                            >
+                                                <Dropdown.Menu>
+                                                    <Dropdown.Item onClick={() => this.rejectInvite('friendSearch', 'cancel')}>
+                                                        Cancel<span className='mob-hide'> friend</span> request
+                                                </Dropdown.Item>
+                                                </Dropdown.Menu>
+                                            </Dropdown>
+                                        </Fragment>
+                                    )}
+                                {(_isEmpty(updatedStatus) || (updatedStatus === 'LIMITED'))
+                                    && (
+                                        <Button
+                                            className={`${buttonClass} c-small`}
+                                            onClick={() => this.addFriend()}
+                                            disabled={buttonState || isBlockedMember}
+                                        >
+                                            {buttonText}
+                                        </Button>
+                                    )}
+                            </Fragment>
                         }
                     </Table.Cell>
                 </Table.Row>
-            </Table.Body>
+            </Table.Body >
         );
     }
 }
@@ -180,7 +291,7 @@ MemberCard.defaultProps = {
         },
         id: '',
     },
-    dispatch: () => {},
+    dispatch: () => { },
     memberData: {
         attributes: {
             avatar: '',
@@ -193,7 +304,7 @@ MemberCard.defaultProps = {
         },
         id: '',
     },
-    t: () => {},
+    t: () => { },
 };
 
 MemberCard.propTypes = {
